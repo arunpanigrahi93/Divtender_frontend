5f55faa22ec7ceee3079a4eccab8a15131acf7f7
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-08-13 10:39:10 +0530

    routing with login profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import Body from "./components/Body";
 import Footer from "./components/Footer";
+import Login from "./components/Login";
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Footer />
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
